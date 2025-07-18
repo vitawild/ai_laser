3f11d817909461fa9b8cc93967ffa74c4eb9f018
vita wild <152096881+vitawild@users.noreply.github.com>
2025-07-18 21:07:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('50.bmp', cv2.IMREAD_GRAYSCALE)
+image = cv2.imread('50.bmp')
 denoised = cv2.GaussianBlur(image, (5, 5), 0)
 
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (65, 65))
@@ -9,8 +9,7 @@ bg = cv2.morphologyEx(denoised, cv2.MORPH_OPEN, kernel)
 
 flat = cv2.subtract(denoised, bg)
 
-c_flat = cv2.cvtColor(flat, cv2.COLOR_GRAY2BGR)
-lab = cv2.cvtColor(c_flat, cv2.COLOR_BGR2LAB)
+lab = cv2.cvtColor(flat, cv2.COLOR_BGR2LAB)
 l, a, b = cv2.split(lab)
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
 l_eq = clahe.apply(l)
